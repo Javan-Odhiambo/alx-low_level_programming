d2c4b92a66f3b184b96a2d8b68b97fffa4cab27d
javan <javan.o.otieno@gmail.com>
2022-12-08 10:41:15 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long int size = sizeof(str) / sizeof(str[0]);
+	long int size = sizeof(str) / sizeof(str[0]) - 1;
 
 	fwrite(str, size, 1, stderr);
 	return (1);
