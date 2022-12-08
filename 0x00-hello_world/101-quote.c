6e869d7524df9c17c4bf2b5945def8c7e44495ed
javan <javan.o.otieno@gmail.com>
2022-12-08 10:38:27 -0800

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Prints a string to standard error
+ * Description: Prints the string below
+ * "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+ * Return: (1)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long int size = sizeof(str) / sizeof(str[0]);
+
+	fwrite(str, size, 1, stderr);
+	return (1);
+}
+
