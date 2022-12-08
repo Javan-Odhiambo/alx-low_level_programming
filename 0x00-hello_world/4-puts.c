d7df992b05d0558c058dc387fb6e68f3bd851db5
javan <javan.o.otieno@gmail.com>
2022-12-08 09:20:10 -0800

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ * Description: Prints the string using puts function.
+ * Return: (0) when successful.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
