56cae8278722a4dfb2bb36c1a308bb5167ebc038
javan <javan.o.otieno@gmail.com>
2022-12-08 09:26:39 -0800

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: Prints the string using printf function.
+ * Return: (0) when successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
