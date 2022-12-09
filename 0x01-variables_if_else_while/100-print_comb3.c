c1c46f3fce0ef6f4adfa0d2c1eae33216a9f5774
javan <javan.o.otieno@gmail.com>
2022-12-09 10:36:32 -0800

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of two digits.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+    
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	    for (j = '0'; j<='9'; j++)
+	    {
+            if (!(j<=i && i!='0'))
+            {
+                putchar(i);
+	            putchar(j);
+	            if (!(i == '8' && j == '9')){
+    	            putchar(',');
+    	            putchar(' ');         
+	            }
+            }
+	    }
+	}
+	putchar('\n');
+	return (0);
+	
+}
