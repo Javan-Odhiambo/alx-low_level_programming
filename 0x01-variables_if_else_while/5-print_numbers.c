d12aad855dcd03316c06b41fff0b39bfd3a1823e
javan <javan.o.otieno@gmail.com>
2022-12-09 09:24:44 -0800

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers 0 to 9.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch = '1';
+
+	for (; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
