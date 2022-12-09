82259f18d84a1c58c58bf31ff3267d0707c78fe3
Javan-Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2022-12-10 02:49:43 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,8 +10,7 @@ int main(void)
 {
 	int i;
 	int j;
-    
-
+	
 	for (i = '0'; i <= '9'; i++)
 	{
 	    for (j = '0'; j<='9'; j++)
@@ -23,12 +22,11 @@ int main(void)
 			    if (!(i == '8' && j == '9'))
 			    {
 			    putchar(',');
-			    putchar(' ');         
+			    putchar(' ');
 			    }
 		    }
 	    }
 	}
 	putchar('\n');
-	return (0);
-	
+	return (0);	
 }
