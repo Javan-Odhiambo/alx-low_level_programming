44b7ada5bffd9bdbd712450397fb65423e03f605
javan <javan.o.otieno@gmail.com>
2022-12-09 16:15:12 -0800

    Update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,13 +19,13 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-			if (!(i == '8' && j == '9'))
-			{
-				putchar(',');
-				putchar(' ');
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-		    }
-	    }
+		}
 	}
 	putchar('\n');
 	return (0);
