981c7d0932e868216d76829d3e03beaff7ad707b
javan <javan.o.otieno@gmail.com>
2022-12-14 09:06:33 -0800

    Create print_alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_aphabet - Prints the alphabet.
+ * Description: Prints the alphabet.
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z', c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
