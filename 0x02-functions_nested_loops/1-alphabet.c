01158a0e00a5910ec45b9226b8df34717d4add66
Javan <javan.o.otieno@gmail.com>
2022-12-14 20:41:22 +0300

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
- * print_aphabet - Prints the alphabet.
- * Description: Prints the alphabet.
- * Return: void.
+ *print_alphabet - Prints the alphabet in lowercase using putchar only once
+ *
+ *
+ *Return: Always zero
  */
-
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
