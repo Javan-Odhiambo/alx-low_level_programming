3e1b2c3275e8e2953327eb2d137466bf2bd7746e
Javan <javan.o.otieno@gmail.com>
2022-12-20 01:45:26 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+ * rev_string - reverses a string.
+ * @s: string to be reversed.
+ *
+ * Return: void.
+*/
+
+void rev_string(char *s)
+{
+	int i, n = 0;
+	char *copy;
+
+	while (*(s + n))
+		n++;
+
+	copy = malloc(n * sizeof(char));
+
+	for (i = 0; i < i; i++)
+	{
+		copy[i] = *(s + n);
+		n--;
+	}
+}
