48eecd376086595ca9889add1a151f95ab83b0d3
javan <javan.o.otieno@gmail.com>
2022-12-19 10:37:58 -0800

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: pointer to an address to be reset.
+ * Return: Void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
