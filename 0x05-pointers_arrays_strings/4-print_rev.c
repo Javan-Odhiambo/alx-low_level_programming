14419fc50d25e4c0af75030267b27beb3140ca92
Javan <javan.o.otieno@gmail.com>
2022-12-20 00:42:45 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: string to be printed in reverse.
+ * Return: void.
+*/
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (*(s + n))
+		n++;
+
+	while (n >= 0)
+		_putchar(*(s + n));
+
+	_putchar('\n');
+}
