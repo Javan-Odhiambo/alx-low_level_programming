ac26f2bd53404f8baaea7d2f9cd1e3e61244e8d3
javan <javan.o.otieno@gmail.com>
2022-12-19 12:47:54 -0800

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+ * _puts - prints a string to stdout.
+ * @str: string to be printed.
+ *
+ * Return: void.
+*/
+
+void _puts(char *str)
+{	
+	int n = 0;
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+	}
+	_putchar('\n');
+}
