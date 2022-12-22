a8293ab1a7679852cae6950f92685e0339be2b0f
javan <javan.o.otieno@gmail.com>
2022-12-21 17:36:26 -0800

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat -  appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte.
+ * @dest: string.
+ * @src: string.
+ * @n: number of bytes to be copied.
+ *
+ * Return: pointer to the resulting string.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int ls = 0, ld = 0, i;
+
+	while (*(dest + ld))
+		ld++;
+
+	while (*(src + ls))
+		ls++;
+
+	for (i = 0; i < n; i++)
+		*(dest + ld + i) = *(src + i);
+
+	return (dest);
+}
