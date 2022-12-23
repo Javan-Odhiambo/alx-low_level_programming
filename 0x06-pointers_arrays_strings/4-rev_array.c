428be7faef65aae6480ee7f6ae41f27e370a7304
Javan <javan.o.otieno@gmail.com>
2022-12-23 04:22:29 +0300

    Update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* reverse_array -  reverses the content of an array of integers.
+*
+* @a: ponter to array.
+* @n: number of elements in array.
+*
+* Return: Void
+*/
+
+void reverse_array(int *a, int n)
+{
+    int temp, i = 0;
+
+	while (i < n)
+	{
+        temp = a[i];
+        a[i] = a[n];
+        a[n] = temp;
+
+        i++;
+        n--;
+	}
+
+}
