1f1d119d816f3468806568a7820fc4f8546d997b
Javan <javan.o.otieno@gmail.com>
2022-12-23 03:44:52 +0300

    Update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 	{
 		if (*(src + i))
 			*(dest + i) = *(src + i);
