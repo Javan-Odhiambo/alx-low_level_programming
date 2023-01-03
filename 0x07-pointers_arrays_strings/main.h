0df9590dcf886993c8cd704189fba7cf8d06cfbc
Javan <javan.o.otieno@gmail.com>
2023-01-04 02:29:26 +0300

    Update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
 #endif
