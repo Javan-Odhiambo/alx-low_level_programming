d48058d74587e3a308eece8719f812bd6c9b1215
Javan <javan.o.otieno@gmail.com>
2023-01-04 01:09:45 +0300

    Update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - function fills the first n bytes of the location pointed to by s
+ * with the constant byte b
+ * @s: pointer to memory location.
+ * @b: byte to be used to fill the spaces.
+ * @n: number of bytes to fill.
+ *
+ * Return: pointer to the memory area s.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s - i - 1);
+}
