c6024da95316046fbb784bc4828c5e80df7e7b1f
Javan <javan.o.otieno@gmail.com>
2023-01-04 01:47:51 +0300

    Update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - looks for a given character in a string.
+ *
+ *@s: string to be searched.
+ *@c: character to be located.
+ *Return: Pointer to first occurence of the character on success.
+ *Else returns NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+
+	return (NULL);
+}
