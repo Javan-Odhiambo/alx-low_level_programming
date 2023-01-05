167830fe2576dc10285626da562dea57f1dbdcf1
Javan-Odhiambo <javan.o.otieno@gmail.com>
2023-01-06 01:59:00 +0300

    Update

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _palindrome - checks if a string is a palindrome
+ * @s: pointer to string to be checked.
+ * @left: pointer to the furthest left of the string.
+ * @right: pointer to the furthest right of the string.
+ * Return: 1 if palindrome else 0.
+ */
+
+_palindrome(char *s, int left, int right)
+{
+	if (left < right)
+	{
+		if (*(s + left) == *(s + right))
+		{
+			_palindrome(s, left++, right--);
+		}
+		else
+		{
+			return (0);
+		}
+
+	}
+	return (1);
+
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to string to be checked
+ * Return: 1 if palindrome else 0.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+
+	int i = 0, end;
+
+	while (*(s + i) != '\0')
+		i++;
+	end = i--;
+
+	return _palindrome(s, 0, end);
+
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * wildcmp - prints a string in reverse
+ * @s: pointer to string to be printed
+ * Return: void
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
