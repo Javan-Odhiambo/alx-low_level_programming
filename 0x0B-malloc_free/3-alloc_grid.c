669f634fe09945153a32ac4d421befde13bdeaf6
Javan <javan.o.otieno@gmail.com>
2023-01-11 01:43:20 +0300

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int j;
 	int **arr = (int **) malloc(height * sizeof(int *));
 
-	if (arr != NULL)d
+	if (arr != NULL)
 	{
 		for (i = 0; i < height; i++)
 		{
