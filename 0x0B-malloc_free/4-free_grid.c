196065095720748693a977161a8cf69fb2328160
Javan <javan.o.otieno@gmail.com>
2023-01-11 02:07:34 +0300

    Update

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,11 +2,18 @@
 #include <malloc.h>
 
 /**
- * free_grid - dfs
- * @grid: 
- * @height:
- * Return: 
+ * free_grid - frees a 2 dimensional grid
+ * @grid: pointer to the grid
+ * @height: height of the grid.
+ * Return: void
 */
 
 
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
