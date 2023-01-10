22b661405ff0132f4c3d1ff27d5720ca3bc31ff2
Javan <javan.o.otieno@gmail.com>
2023-01-11 00:04:00 +0300

    Update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <malloc.h>
+#include <string.h>
 
 /**
  * str_concat - concatenates two strings.
@@ -15,8 +16,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int size;
 	int i;
 	int j;
-	int s1_len = 0;
-	int s2_len = 0;
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
 
 	size = s1_len + s2_len + 1;
 
