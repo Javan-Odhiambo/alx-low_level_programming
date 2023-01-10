352e1de9753d28973c91876cfe6e658d4de1df1b
Javan <javan.o.otieno@gmail.com>
2023-01-11 00:06:08 +0300

    Update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int size;
 	int i;
 	int j;
-	int s1_len = strlen(s1);
-	int s2_len = strlen(s2);
+	int s1_len = (s1_len == NULL) ? 0 : strlen(s1);
+	int s2_len = (s2_len == NULL) ? 0 : strlen(s2);
 
 	size = s1_len + s2_len + 1;
 
