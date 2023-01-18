8ae9a8965dfedd2bfa0ff17572023377464850a6
Javan <javan.o.otieno@gmail.com>
2023-01-19 01:49:17 +0300

    Project Update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,14 @@ void print_dog(struct dog *d)
 	{
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
-        else
+		else
 			printf("Name: (nil)\n");
 
 		printf("Age: %f\n", d->age);
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
-        else
+		else
 			printf("Owner: (nil)\n");
 	}
 
