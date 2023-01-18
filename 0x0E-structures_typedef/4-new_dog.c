a6b5f54ee8548fc2d58948a00812ed58482ed928
Javan <javan.o.otieno@gmail.com>
2023-01-19 01:58:11 +0300

    Project Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
 
+/**
+ * new_dog - create a new dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the dog struct
+*/
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
