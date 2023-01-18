6740c74ba4bbe5974d269a878693f90499fe507c
Javan <javan.o.otieno@gmail.com>
2023-01-19 02:19:51 +0300

    Project Update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
 
+/**
+ * free_dog - frees dog memory
+ * @d: pointer to memory being freed
+ * Return: 0
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
