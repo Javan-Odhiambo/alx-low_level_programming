4f0e93322c3e813088918a748caa0e3dcee03089
Javan <javan.o.otieno@gmail.com>
2023-01-19 02:10:39 +0300

    Project Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,10 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog = malloc(sizeof(dog_t));
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
-	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
