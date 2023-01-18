c4c1357da4f62655f37c32d4382053ba795a60c6
Javan <javan.o.otieno@gmail.com>
2023-01-19 01:44:41 +0300

    Project Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,29 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - A struct representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for dog
+*/
+typedef struct dog dog_t;
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+#endif
