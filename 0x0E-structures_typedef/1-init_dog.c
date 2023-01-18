613d92b592cdecd063af285822ececa1b210a346
Javan <javan.o.otieno@gmail.com>
2023-01-19 01:37:16 +0300

    Project Update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - Initialize a vatiable of type struct dog.
+ * @d: pointer to the dog struct.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Return: void.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+    	d->age = age;
+		d->owner = owner;
+	}
+
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the dog struct.
+ * @d: pointer to the dog struct
+ * Return: 0
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+        else
+			printf("Name: (nil)\n");
+
+		printf("Age: %s\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+        else
+			printf("Owner: (nil)\n");
+	}
+
+}
