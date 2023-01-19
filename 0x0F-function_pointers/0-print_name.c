ee8337bf8cd168046060dcaa6cafe52d3f8f38a8
Javan <javan.o.otieno@gmail.com>
2023-01-19 12:41:55 +0300

    Project Update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: name to be printed
- * @f: function to be called
- * Returns: void
-*/
+ * @f: function to be called to print the name
+ * Return: void
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		(*f)(name);
+		f(name);
 }
