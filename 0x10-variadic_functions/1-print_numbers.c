7575113558f1c8791558f781b0433602e511e18f
Javan <javan.o.otieno@gmail.com>
2023-01-20 13:23:07 +0300

    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,5 +10,17 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != n-1)
+			printf("%s ", separator);
+	}
+	putchar('\n');
+
+	va_end(args);
+	return;
 }
