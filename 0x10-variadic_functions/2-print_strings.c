369e641b53d975dcfc1f719104fae04282842914
Javan <javan.o.otieno@gmail.com>
2023-01-20 13:32:26 +0300

    To be is to be the value of a variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,13 +2,23 @@
 
 /**
  * print_strings - 
- * @separator: 
- * @n: 
- * Description: 
- * Return:
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * Return: void
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list args;
 
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(args);
 }
