210a85f5d2a853ccc8cbe26518e5256fca4c445f
Javan <javan.o.otieno@gmail.com>
2023-01-30 21:52:09 -0500

    Update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: link to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 
 size_t print_list(const list_t *h);
