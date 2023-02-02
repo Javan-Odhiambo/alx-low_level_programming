34e27a3ca221f22a1ed244ba15c10302b7c21f6e
Javan <javan.o.otieno@gmail.com>
2023-02-02 17:28:52 +0300

    Update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,20 @@
-void free_listint2(listint_t **head);
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list.
+ * @head: head of the linked list.
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+}
