b5b4704d470a4ba81f2d98df980fcd0129f31c2a
Javan <javan.o.otieno@gmail.com>
2023-02-02 16:19:24 +0300

    Update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * listint_len -  counts the number of elements in the linked list
+ * @h: list to be printed.
+ *
+ * Return: number of elements in the single linked list
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t n = 0;
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,13 @@
 #include"lists.h"
 
+/**
+ * add_nodeint -  Adds a node to the end of the linked list.
+ * @head: head of the linked list.
+ * @n: data to be placed in the new list.
+ *
+ * Return: pointer to newly created node or NULL
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *pNode = (listint_t *) malloc(sizeof(listint_t));
