5417548d2fc447ff35143c942a65b788efac7b14
Javan <javan.o.otieno@gmail.com>
2023-02-02 18:19:52 +0300

    Update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,21 @@
-int sum_listint(listint_t *head);
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: head of the linked list.
+ * Return: sum of data (n) or (0) when empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *tmp = NULL;
+	int sum;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		sum += tmp->n;
+	}
+
+	return (sum);
+}
+
