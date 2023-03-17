3633c6156227eb043c8714d2f66ce99f89857791
Javan <javan.o.otieno@gmail.com>
2023-03-17 11:58:45 +0300

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,23 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints a doubly linked list.
+ * @h:head of the linked list.
+ * Return: size of the doubly linked list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;
+	size_t n = 0;
 
-    if (h == NULL)
-        return (n);
+	if (h == NULL)
+		return (n);
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        n++;
-        h = h->next;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
 
-    return (n);
+	return (n);
 }
