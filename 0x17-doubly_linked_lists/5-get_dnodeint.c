944811c57d2f53a9476e3e11f8512188ac50fe85
Javan <javan.o.otieno@gmail.com>
2023-03-17 15:26:02 +0300

    Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Prints a doubly linked list.
+ * @head:head of the linked list.
+ * @index:index to get the node.
+ * Return: pointer to node or NULL if node doesn't exit.
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *trav;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	trav = head;
+	i = 0;
+
+	while (trav != NULL && i < index)
+	{
+		trav = trav->next;
+		i++;
+	}
+
+	if (i == index)
+		return(trav);
+	else
+		return (NULL);
+
+}
