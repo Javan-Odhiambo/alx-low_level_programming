e92420eecd1d134f225e64a9884e4df676f268b8
Javan <javan.o.otieno@gmail.com>
2023-04-12 23:06:09 +0300

    finished

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,55 +1,73 @@
 #include "hash_tables.h"
-#include <string.h>
-
 
 /**
- * create_node - creates a node for the hash table.
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
  *
- * @key: Key of the node.
- * @value: Value to be stored in the node.
- *
- * Return: Pointer to the created node on success or NULL on failure.
-*/
-
-hash_node_t * create_node(const char *key, const char *value)
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
 {
-	hash_node_t *node;
-	
-	node = (hash_node_t *) malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
-
-	return (node);
+    hash_node_t *node;
+  
+    node = malloc(sizeof(hash_node_t));
+    if (node == NULL)
+        return (NULL);
+    node->key = strdup(key);
+    if (node->key == NULL)
+    {
+        free(node);
+        return (NULL);
+    }
+    node->value = strdup(value);
+    if (node->value == NULL)
+    {
+        free(node);
+        free(node->key);
+        return (NULL);
+    }
+    node->next = NULL;
+    return (node);
 }
 
 /**
- * hash_table_set - adds an element to the hash table.
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add element to
+ * @key: key for the data
+ * @value: data to store
  *
- * @ht: The hash table to insert to.
- * @key: The key of the item.
- * @value: The value of the item.
- *
- * Return: 1 on success and 0 on failure.
-*/
-
+ * Return: 1 if successful, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+    unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
-	unsigned long int index;
-	hash_node_t *node;
-
-	if (strlen(key) == 0 || key == NULL || ht == NULL ||
-		   	ht->array == NULL || ht->size == 0 || value == NULL)
-		return (0);
-
-	node = create_node(key, value);
-	index = key_index((const unsigned char *) key, ht->size);
-
-	if (ht->array[index] != NULL)
-		node->next = ht->array[index];
-
-	ht->array[index] = node;
-	return (1);
+    if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
+        return (0);
+    index = key_index((const unsigned char *)key, ht->size);
+    tmp = ht->array[index];
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
+            new_value = strdup(value);
+            if(new_value == NULL)
+                return (0);
+            free(tmp->value);
+            tmp->value = new_value;
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+    hash_node = make_hash_node (key, value);
+    if (hash_node == NULL)
+    
+        return (0);
+    hash_node->next = ht->array[index];
+    ht->array[index] = hash_node;
+    return (1);   
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,6 +2,8 @@
 #define _HASH_TABLES_H_
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -13,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+     char *key;
+     char *value;
+     struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,13 +30,16 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+     unsigned long int size;
+     hash_node_t **array;
 } hash_table_t;
-
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-#endif /* _HASH-TABLES_H_ */
+#endif /*_HASH_TABLES_H_*/
