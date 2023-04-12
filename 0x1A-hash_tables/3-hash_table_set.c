11480296e4c0891f4a996788a87e11136ad4f349
Javan <javan.o.otieno@gmail.com>
2023-04-12 23:09:25 +0300

    finished

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,26 +9,26 @@
  */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-    hash_node_t *node;
-  
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL)
-        return (NULL);
-    node->key = strdup(key);
-    if (node->key == NULL)
-    {
-        free(node);
-        return (NULL);
-    }
-    node->value = strdup(value);
-    if (node->value == NULL)
-    {
-        free(node);
-        free(node->key);
-        return (NULL);
-    }
-    node->next = NULL;
-    return (node);
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node);
+		free(node->key);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
 }
 
 /**
@@ -41,33 +41,33 @@ hash_node_t *make_hash_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
+	unsigned long int index;
 	hash_node_t *hash_node, *tmp;
 	char *new_value;
 
-    if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	    key == NULL || strlen(key) == 0 || value == NULL)
-        return (0);
-    index = key_index((const unsigned char *)key, ht->size);
-    tmp = ht->array[index];
-    while (tmp != NULL)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            new_value = strdup(value);
-            if(new_value == NULL)
-                return (0);
-            free(tmp->value);
-            tmp->value = new_value;
-            return (1);
-        }
-        tmp = tmp->next;
-    }
-    hash_node = make_hash_node (key, value);
-    if (hash_node == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
 
-        return (0);
-    hash_node->next = ht->array[index];
-    ht->array[index] = hash_node;
-    return (1);
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
 }
